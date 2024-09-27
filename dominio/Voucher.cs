e75f23bbc1771013c99ea583ad9c5e7f2e1cfece
CharlieMarques <Charlie.marques1806@gmail.com>
2024-09-27 00:11:29 -0300

    agrego clases voucher y cliente

---
+++ dominio/Voucher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Voucher
+    {
+        public string Codigo { get; set; }
+        public int idCliente { get; set; }
+        public DateTime FechaCanje { get; set; }
+        public int idArticulo { get; set; }
+    }
+}
